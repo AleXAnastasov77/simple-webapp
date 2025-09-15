0f82191e03ad50f5059570bc14ebae5b79f36c6e
Anastasov,Aleks A.O. <a.anastasov@student.fontys.nl>
2025-09-15 12:45:53 +0200

    change project setup

---
+++ app/app.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from flaskext.mysql import MySQL      # For newer versions of flask-mysql 
+# from flask.ext.mysql import MySQL   # For older versions of flask-mysql
+app = Flask(__name__)
+
+mysql = MySQL()
+
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = os.getenv("DB_USER")
+app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv("DB_PASSWORD")
+app.config['MYSQL_DATABASE_DB'] = os.getenv("DB_NAME")
+app.config['MYSQL_DATABASE_HOST'] = os.getenv("DB_HOST")
+mysql.init_app(app)
+
+conn = mysql.connect()
+
+cursor = conn.cursor()
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
+@app.route('/read from database')
+def read():
+    cursor.execute("SELECT * FROM employees")
+    row = cursor.fetchone()
+    result = []
+    while row is not None:
+      result.append(row[0])
+      row = cursor.fetchone()
+
+    return ",".join(result)
+
+if __name__ == "__main__":
+    app.run()
