d6a11d47c0dbc299e30f03e7fef126fb8d39a025
Anastasov,Aleks A.O. <a.anastasov@student.fontys.nl>
2025-09-16 16:15:00 +0200

    Gateways, route tables, rt associations

--- infra/network.tf
+++ infra/network.tf
@@ -6,7 +6,7 @@ resource "aws_vpc" "vpc_cs1" {
     Name = "vpc_cs1"
   }
 }
-
+# ////////////////////// SUBNETS //////////////////////////
 resource "aws_subnet" "public_cs1_A" {
   vpc_id                  = aws_vpc.vpc_cs1.id
   cidr_block              = "10.0.1.0/24"
@@ -58,3 +58,76 @@ resource "aws_subnet" "privatedb_cs1_B" {
     Name = "privatedb_cs1_B"
   }
 }
+
+
+# ////////////////////// GATEWAYS //////////////////////////
+resource "aws_internet_gateway" "igw_cs1" {
+  vpc_id = aws_vpc.vpc_cs1.id
+
+  tags = {
+    Name = "igw_cs1"
+  }
+}
+
+resource "aws_nat_gateway" "natgw_cs1" {
+  subnet_id     = aws_subnet.public_cs1_A.id
+
+  tags = {
+    Name = "natgw_cs1"
+  }
+  depends_on = [aws_internet_gateway.igw_cs1]
+}
+
+# ////////////////////// ROUTE TABLES //////////////////////////
+
+resource "aws_route_table" "rt_public_cs1" {
+  vpc_id = aws_vpc.vpc_cs1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_cs1.id
+  }
+  tags = {
+    Name = "rt_public_cs1"
+  }
+}
+
+resource "aws_route_table" "rt_private_cs1" {
+  vpc_id = aws_vpc.vpc_cs1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.natgw_cs1.id
+  }
+  tags = {
+    Name = "rt_private_cs1"
+  }
+}
+
+# ////////////////////// ROUTE TABLE ASSOCIATIONS //////////////////////////
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_cs1_A.id
+  route_table_id = aws_route_table.rt_public_cs1.id
+}
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public_cs1_B.id
+  route_table_id = aws_route_table.rt_public_cs1.id
+}
+resource "aws_route_table_association" "c" {
+  subnet_id      = aws_subnet.privatedb_cs1_A.id
+  route_table_id = aws_route_table.rt_private_cs1.id
+}
+resource "aws_route_table_association" "d" {
+  subnet_id      = aws_subnet.privatedb_cs1_B.id
+  route_table_id = aws_route_table.rt_private_cs1.id
+}
+resource "aws_route_table_association" "e" {
+  subnet_id      = aws_subnet.privateweb_cs1_A.id
+  route_table_id = aws_route_table.rt_private_cs1.id
+}
+resource "aws_route_table_association" "f" {
+  subnet_id      = aws_subnet.privateweb_cs1_B.id
+  route_table_id = aws_route_table.rt_private_cs1.id
+}
+
