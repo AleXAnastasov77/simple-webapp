99a2b0333ea8dc3871f4e2337def7ffff06240d4
Anastasov,Aleks A.O. <a.anastasov@student.fontys.nl>
2025-09-23 12:29:32 +0200

    fix security policy for app sg back to port 5000

--- infra/network.tf
+++ infra/network.tf
@@ -166,6 +166,13 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
+    description = "Allow HTTP"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
     description = "Allow Node Exporter"
     from_port   = 9100 
     to_port     = 9100
