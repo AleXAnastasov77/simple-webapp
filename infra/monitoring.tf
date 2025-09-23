9b89165dde6cb2794deed6ac5266d40f289f380f
Anastasov,Aleks A.O. <a.anastasov@student.fontys.nl>
2025-09-23 12:25:10 +0200

    change the role of the ec2 instance to allow for dynamic discovery

--- infra/monitoring.tf
+++ infra/monitoring.tf
@@ -14,13 +14,15 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+
 resource "aws_instance" "monitoring_ec2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.small"
-  iam_instance_profile = "SSMRole"
+  iam_instance_profile = "MonitoringRole"
   subnet_id = aws_subnet.privatemonitoring_cs1_B.id
   vpc_security_group_ids = [aws_security_group.monitoring_sg.id]
   key_name = "ansible-keypair"
+  
   root_block_device {
     delete_on_termination = false
     volume_size = 25
