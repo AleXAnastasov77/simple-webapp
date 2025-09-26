59e6a078a2cd25b05948345b9e48e91f0414db1c
Anastasov,Aleks A.O. <a.anastasov@student.fontys.nl>
2025-09-26 09:33:30 +0200

    add back load balancer

--- infra/webapp.tf
+++ infra/webapp.tf
@@ -52,23 +52,23 @@ resource "aws_lb_target_group" "lbtg_cs1" {
   }
 }
 
-# resource "aws_lb" "alb_cs1" {
-#   name               = "alb-cs1"
-#   load_balancer_type = "application"
-#   subnets            = [aws_subnet.public_cs1_A.id, aws_subnet.public_cs1_B.id]
-#   security_groups = [aws_security_group.app_sg.id]
-# }
+resource "aws_lb" "alb_cs1" {
+  name               = "alb-cs1"
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public_cs1_A.id, aws_subnet.public_cs1_B.id]
+  security_groups = [aws_security_group.app_sg.id]
+}
 
-# resource "aws_lb_listener" "http" {
-#   load_balancer_arn = aws_lb.alb_cs1.arn
-#   port              = 80
-#   protocol          = "HTTP"
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb_cs1.arn
+  port              = 80
+  protocol          = "HTTP"
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.lbtg_cs1.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lbtg_cs1.arn
+  }
+}
 
 resource "aws_autoscaling_group" "bar" {
   vpc_zone_identifier       = [aws_subnet.privateweb_cs1_A.id, aws_subnet.privateweb_cs1_B.id]
